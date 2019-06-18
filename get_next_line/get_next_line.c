5bd223f117cd018759a7f614dee2130a9393d922
Luis Sanchez <lusanche@e1z3r1p12.42.us.org>
2019-06-17 23:08:45 -0700

    third backup

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,11 @@
 /*   By: lusanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 21:22:01 by lusanche          #+#    #+#             */
-/*   Updated: 2019/06/15 22:33:41 by lusanche         ###   ########.fr       */
+/*   Updated: 2019/06/16 22:39:11 by lusanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-
 
 int		get_next_line(const int fd, char **line)
 {	
@@ -22,55 +20,38 @@ int		get_next_line(const int fd, char **line)
 	char		*nl;
 	int			n;
 
-//	ft_putendl("--------------------------------------");
-	if (BUFF_SIZE < 0)
+	if (fd < 0 || line == NULL)
 		return (-1);
 	if (!store)
-		store = ft_stranew(5);
+		store = ft_stranew(65535);
 	n = 0;
-//											printf("[STORE]%s\n", store);
 	if (!store[fd])
-		store[fd] = ft_strnew(0);
-//											printf("[STORE]%s\n", store);
+		store[fd] = ft_strnew(1);
 	while (!(nl = ft_strchr(store[fd], '\n')))
 	{
 		buf = ft_strnew(BUFF_SIZE);
-//											printf("[BUF]%s\n", buf);
 		n = read(fd, buf, BUFF_SIZE);
-//											printf("[N]%d\n", n);
-//											printf("[BUF]%s\n", buf);
-		
-		if (!n && *store[fd] == '\0')
+		if (n < 0)
+			return (-1);
+		if (!n && (*store[fd] == '\0' || store[fd] == NULL))
 		{	
 			*line = ft_strdup(store[fd]);
+			ft_strdel(line);
 			ft_strdel(&store[fd]);
 			return (0);
 		}
-		
 		temp = ft_strdup(store[fd]);
-//											printf("[TEMP]%s\n", temp);
 		ft_strdel(&store[fd]);
-//											printf("[STORE]%s\n", store);
 		store[fd] = ft_strjoin(temp, buf);
-//											printf("[STORE]%s\n", store);
 		ft_strdel(&buf);
-//											printf("[BUF]%s\n", buf);
 		ft_strdel(&temp);
-//											printf("[TEMP]%s\n", temp);
 	}
-//											printf("[STORE]%s\n", store);
 	*nl = '\0';
 	*line = ft_strdup(store[fd]);
-//											printf("[*LINE]%s\n", *line);
 	++nl;
-//											printf("[NL]%s\n", nl);
 	temp = ft_strdup(nl);
-//											printf("[TEMP]%s\n", temp);
 	ft_strdel(&store[fd]);
-//											printf("[STORE]%s\n", store);
 	store[fd] = ft_strdup(temp);
-//											printf("[STORE]%s\n", store);
 	ft_strdel(&temp);
-//											printf("[TEMP]%s\n", temp);
 	return (1);
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,12 +6,13 @@
 /*   By: lusanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 21:22:41 by lusanche          #+#    #+#             */
-/*   Updated: 2019/06/15 22:43:21 by lusanche         ###   ########.fr       */
+/*   Updated: 2019/06/16 22:36:28 by lusanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int		main(void)
 {
@@ -21,18 +22,26 @@ int		main(void)
 	int		ret1;
 	int		ret2;
 
+	ret1 = 1;
+	ret2 = 1;
 	fd1 = open("../Quixote.txt", O_RDONLY, 0);
 	fd2 = open("../Iliad.txt", O_RDONLY, 0);
-	while ((ret1 = get_next_line(fd1, &ptr) > 0) ||\
-			(ret2 = get_next_line(fd2, &ptr) > 0))
+	while (ret1 > 0 || ret2 > 0)
 	{
-		ft_putstr("[Q]");
-		ft_putendl(ptr);
-		free(ptr);
-		ret2 = get_next_line(fd2, &ptr);
-		ft_putstr("[I]");
-		ft_putendl(ptr);
-		free(ptr);
+		if (ret1)
+		{
+			ret1 = get_next_line(fd1, &ptr);
+			printf("[Q]");
+			printf("%s\n", ptr);
+			free(ptr);
+		}
+		if (ret2)
+		{
+			ret2 = get_next_line(fd2, &ptr);
+			printf("[I]");
+			printf("%s\n", ptr);
+			free(ptr);
+		}
 	}
 	return (0);
 }
