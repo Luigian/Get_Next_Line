dd9bef21b15134194b8e0afeb084cbcb952d9465
Luis Sanchez <lusanche@e1z3r1p12.42.us.org>
2019-06-22 16:29:38 -0700

    tested

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,69 +6,56 @@
 /*   By: lusanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 21:22:01 by lusanche          #+#    #+#             */
-/*   Updated: 2019/06/21 17:48:34 by lusanche         ###   ########.fr       */
+/*   Updated: 2019/06/22 16:10:53 by lusanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		if_no_read(int n, char **s, const int f, char **l)
+void	final(char *nl, char **s, int f, char **l)
 {
-	if (n < 0)
-		return (-1);
-	if (!n && (*s[f] == '\0' || s[f] == NULL))
-	{
-		*l = ft_strdup(s[f]);
-		ft_strdel(l);
-		ft_strdel(&s[f]);
-		return (0);
-	}
-	else if (!n)
+	char	*t;
+
+	if (!(nl = ft_strchr(s[f], '\n')))
 	{
 		*l = ft_strdup(s[f]);
 		ft_strdel(&s[f]);
-		return (1);
 	}
-	return (2);
-}
-
-int		build_until_nl(const int f, char **s, char **l, char *t)
-{
-	char		*buf;
-	int			n;
-	int			e;
-
-	buf = ft_strnew(BUFF_SIZE);
-	n = read(f, buf, BUFF_SIZE);
-	if ((e = if_no_read(n, s, f, l)) != 2)
-		return (e);
-	t = ft_strdup(s[f]);
-	ft_strdel(&s[f]);
-	s[f] = ft_strjoin(t, buf);
-	ft_strdel(&buf);
-	ft_strdel(&t);
-	return (2);
+	else
+	{
+		*nl++ = '\0';
+		*l = ft_strdup(s[f]);
+		t = ft_strdup(nl);
+		ft_strdel(&s[f]);
+		s[f] = t;
+	}
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	char		*temp;
-	static char	**store;
-	char		*nl;
-	int			r;
+	char			buf[BUFF_SIZE + 1];
+	static char		*store[65535];
+	char			*temp;
+	char			*nl;
+	int				n;
 
-	if (fd < 0 || line == NULL)
+	while (line && fd >= 0 && ((n = read(fd, buf, BUFF_SIZE)) > 0))
+	{
+		!store[fd] ? store[fd] = ft_strnew(1) : 0;
+		buf[n] = '\0';
+		temp = ft_strjoin(store[fd], buf);
+		ft_strdel(&store[fd]);
+		store[fd] = temp;
+		if ((nl = ft_strchr(store[fd], '\n')))
+			break ;
+	}
+	if (n < 0 || fd < 0 || line == NULL)
 		return (-1);
-	!store ? store = ft_stranew(65535) : 0;
-	!store[fd] ? store[fd] = ft_strnew(1) : 0;
-	while (!(nl = ft_strchr(store[fd], '\n')))
-		if ((r = build_until_nl(fd, store, line, temp)) != 2)
-			return (r);
-	*nl++ = '\0';
-	*line = ft_strdup(store[fd]);
-	temp = ft_strdup(nl);
-	ft_strdel(&store[fd]);
-	store[fd] = ft_strdup(temp);
-	ft_strdel(&temp);
+	if (!n && (store[fd] == NULL || *store[fd] == '\0'))
+	{
+		ft_strdel(&store[fd]);
+		return (0);
+	}
+	final(nl, store, fd, line);
 	return (1);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lusanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 21:21:28 by lusanche          #+#    #+#             */
-/*   Updated: 2019/06/21 15:07:48 by lusanche         ###   ########.fr       */
+/*   Updated: 2019/06/22 13:23:33 by lusanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # include "./libft/libft.h"
-# define BUFF_SIZE 1
+# define BUFF_SIZE 10
 
 int		get_next_line(const int fd, char **line);
 
