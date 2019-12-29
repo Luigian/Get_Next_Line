fd509efdf0ccffc5cc23faa7e9bc90990b310aa1
Luis Sanchez <loic.quiros@gmail.com>
2019-12-28 20:59:34 -0800

    order

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lusanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 21:21:28 by lusanche          #+#    #+#             */
-/*   Updated: 2019/06/22 20:29:58 by lusanche         ###   ########.fr       */
+/*   Updated: 2019/06/26 11:21:49 by lusanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # include "./libft/libft.h"
-# define BUFF_SIZE 1
+# define BUFF_SIZE 9999
 
 int		get_next_line(const int fd, char **line);
 
